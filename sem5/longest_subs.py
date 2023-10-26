74f5c0a8823d695c211b966f61cc8e9e5e04239d
EchoPr <arkasha.pronin.2005@gmail.com>
2023-10-26 23:27:28 +0300

    fixed ls

---
+++ sem5/longest_subs.py
@@ -0,0 +1,30 @@
+import string
+
+
+def lengthOfLongestSubstring(s: str) -> int:
+    t = ''
+    mp = dict(zip(s, [-1] * len(s)))
+    mx = 0
+    n = len(s)
+    start = 0
+
+    for i in range(len(s)):
+        if mp[s[i]] != -1:
+            mx = max(mx, len(t) - start)
+            pos = mp[s[i]]
+
+            start = max(pos + 1, start)
+                
+        t += s[i]
+        mp[s[i]] = i
+
+    mx = max(mx, len(t) - start)
+    return mx
+    
+
+if __name__ == "__main__":
+    assert lengthOfLongestSubstring('abcabcbb') == 3
+    assert lengthOfLongestSubstring('bbbbb') == 1
+    assert lengthOfLongestSubstring('pwwkew') == 3     # 'pwke' является подпоследовательностью, 
+    assert lengthOfLongestSubstring('tmmzuxt') == 5  
+    assert lengthOfLongestSubstring('bbtablud') == 6                                             # но не подсловом
