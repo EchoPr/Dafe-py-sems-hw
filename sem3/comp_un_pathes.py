1f143cbbcb171376fd8678fad00e0b30ba6d2462
EchoPr <arkasha.pronin.2005@gmail.com>
2023-10-26 19:46:03 +0300

    fixed comp_un_pathes

--- sem3/comp_un_pathes.py
+++ sem3/comp_un_pathes.py
@@ -1,37 +1,28 @@
-
-# Передалал старт рекурсии, тк могла выходить лажа
-# Считаю количество путей в отрицательных числах, чтобы не считать стены за клетку, в которую ведёт один путь
-
-def make_step(grid, i, j):
-
-    up, left = (grid[i - 1][j] if i > 0 and grid[i - 1][j] != 1 else 0), \
-                 (grid[i][j - 1] if j > 0 and grid[i][j - 1] != 1 else 0)
-    
-    grid[i][j] = up + left
-    if i == j == 0:
-        grid[i][j] = -1
-
-    if i < len(grid) - 1 and grid[i + 1][j] != 1:
-        make_step(grid, i + 1, j)
-
-    if j < len(grid[0]) - 1 and grid[i][j + 1] != 1:
-        make_step(grid, i, j + 1)
-                 
-
 def compute_unique_pathes(grid:list[list[int]]) -> int:
-    grid[0][0] = -1
+    if grid[0][0] == 1 or grid[-1][-1] == 1: 
+        return 0 
 
-    make_step(grid, 0, 0)
+    for i in range(len(grid[0])):
+        if grid[0][i] != 1: grid[0][i] = -1
+    for i in range(len(grid)):
+        if grid[i][0] != 1: grid[i][0] = -1
+
+    for i in range(1, len(grid)):
+        for j in range(1, len(grid[0])):
+            if grid[i][j] == 1: continue
+            
+            grid[i][j] = (
+                grid[i - 1][j] * (grid[i - 1][j] != 1) +
+                grid[i][j - 1] * (grid[i][j - 1] != 1)
+            )
 
     return abs(grid[-1][-1])
 
 grid = [
- [0, 0, 0],
- [0, 1, 0], 
- [0, 0, 0],
- [0, 1, 0]
+    [0, 1],
+    [1, 0]
 ]
 
 ans = compute_unique_pathes(grid)
 print(*grid, sep='\n')
-assert ans == 2
+assert ans == 0
